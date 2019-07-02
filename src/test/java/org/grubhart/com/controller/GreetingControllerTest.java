c01b37f927a3ceb8b919f210e9d9f63cd4837cfa
Grubhart <grubhart@gmail.com>
2019-07-02 16:19:33 -0500

    Added Test for the Endpoint with parameters

--- src/test/java/org/grubhart/com/controller/GreetingControllerTest.java
+++ src/test/java/org/grubhart/com/controller/GreetingControllerTest.java
@@ -28,4 +28,11 @@ public class GreetingControllerTest {
                 .andExpect(jsonPath("$.message").value("Greeting  world!"));
     }
 
+    @Test
+    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Greeting Spring Community!"));
+    }
+
 }
